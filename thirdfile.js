b2329a50ce4bc48315ad566507e1a52bdfa2fcec
ArunK235 <arunlanda235>
2022-10-15 23:39:31 +0530

    adding thirdfile

---
+++ thirdfile.js
@@ -0,0 +1,18 @@
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+//items[1].textContent='hello';
+//items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='yellow';
+
+//items.style.backgroundColor='#f4f4f4'; it gives error
+//for(var i=0; i<items.length;i++){
+    //items[i].style.backgroundColor='#f4f4f4';
+//}
+
+items[2].style.backgroundColor='green';
+for(var i=0; i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+    items[i].style.fontWeight='bold';
+    
+}
